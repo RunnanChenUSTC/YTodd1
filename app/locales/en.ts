f68987d71feedc8884d4ecd76a53055d376573ef
ChenRunnanUSTC <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-01-31 11:04:00 +0800

    Update en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -10,7 +10,7 @@ const en: LocaleType = {
   Error: {
     Unauthorized: isApp
       ? "Invalid API Key, please check it in [Settings](/#/settings) page."
-      : "Unauthorized access, please enter access code in [auth](/#/auth) page!",
+      : "Unauthorized access, please enter access code in [auth](/#/auth) page.",
   },
   Auth: {
     Title: "Need Access Code",
